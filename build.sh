824f7ce3551b85009fe740aeb93db263f86d9480
kn06497 <kn06497@hotmail.com>
2016-10-23 12:46:04 -0500

    d
    Signed-off-by: kn06497 <kn06497@hotmail.com>

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ccache
+export USE_CCACHE=1
+mkdir -d /home/android/ccache/cm_$device
+export CCACHE_DIR=/home/android/ccache/cm_$device
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+
+
+# kbuild flags
+export KBUILD_BUILD_USER=Kn06497
+export KBUILD_BUILD_HOST=xda-developers
+
+# Build environment
+. build/envsetup.sh
+brunch $device
