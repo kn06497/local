9601938e7edeb01e37560f2a27f5f86c2d9ff43b
Kevin Nuñez Banderas <kn06497@hotmail.com>
2016-11-16 03:18:12 +0100

    Update build_jf.sh

--- build_jf.sh
+++ build_jf.sh
@@ -2,14 +2,14 @@
 
 # ccache
 export USE_CCACHE=1
-mkdir -d /home/android/ccache/aicp_$device
-export CCACHE_DIR=/home/android/ccache/aicp_$device
-prebuilts/misc/linux-x86/ccache/ccache -M 50G
+mkdir -d /home/android/ccache/cm_$device
+export CCACHE_DIR=/home/android/ccache/cm_$device
+prebuilts/misc/linux-x86/ccache/ccache -M 20G
 
 
 # kbuild flags
 export KBUILD_BUILD_USER=Kn06497
-export KBUILD_BUILD_HOST=xda-developers
+export KBUILD_BUILD_HOST=kn06497
 
 # Build environment
 . build/envsetup.sh
