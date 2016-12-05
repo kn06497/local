f338f80b52d4a41a5570f2a77217eaec1d0fc05b
Kevin Nuñez Banderas <kn06497@hotmail.com>
2016-12-05 17:33:10 -0500

    Update build_jf.sh

--- build_jf.sh
+++ build_jf.sh
@@ -25,17 +25,17 @@ DEVICE="$1"
 
 
 # ccache
-export USE_CCACHE=1
-mkdir -p /home/jenkins/ccache/cm_$device
-export CCACHE_DIR=/home/jenkins/ccache/cm_$device
-prebuilts/misc/linux-x86/ccache/ccache -M 20G
+# export USE_CCACHE=1
+# mkdir -p /home/jenkins/ccache/cm_$device
+# export CCACHE_DIR=/home/jenkins/ccache/cm_$device
+# prebuilts/misc/linux-x86/ccache/ccache -M 20G
 
 # Compile the build
-	export CM_BUILDTYPE=NIGHTLY
+#	export CM_BUILDTYPE=NIGHTLY
 	. build/envsetup.sh
 	brunch $device
 
 # Upload 
-home="/home/jenkins"
-rom="Android/cm-14.1/out/target/product/*for*/*2016*.zip"
+home="/home/build2"
+rom="RR-N/out/target/product/fortuna*/*2016*.zip"
 curl -T $rom ftp://uploads.androidfilehost.com --user kn06497:YKAcLO5U0vyD
