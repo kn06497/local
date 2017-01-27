17e7689495b250ad3bdb297f9414087dd3c986e3
Kevin Nuñez Banderas <kn06497@hotmail.com>
2017-01-27 12:44:58 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 # ccache
 export USE_CCACHE=1
-mkdir -p /var/lib/jenkins/ccache/cm_$device
-export CCACHE_DIR=/var/lib/jenkins/ccache/cm_$device
-prebuilts/misc/linux-x86/ccache/ccache -M 20G
+mkdir -p /home/kn06497/.ccache/cm_ghost
+export CCACHE_DIR=/home/kn06497/.ccache/cm_ghost
+prebuilts/misc/linux-x86/ccache/ccache -M 30G
 
 # Compile the build
 	. build/envsetup.sh
-	brunch $device
+	export WITH_SU=true
+	export CM_BUILDTYPE=EXPERIMENTAL
+	brunch ghost
 
 # Upload 
 
-rom="out/target/product/*fortuna/*2016*.zip"
-curl -T $rom ftp://uploads.androidfilehost.com --user kn06497:YKAcLO5U0vyD
+curl -T out/target/product/ghost/lineage-*.zip ftp://uploads.androidfilehost.com --user kn06497:YKAcLO5U0vyD
